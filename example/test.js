1943aad03f56f3bbaa80cf33ba8e5ce9cd884e00
wxshenzhenpeng <Szpshr123>
2017-12-07 18:04:38 +0800

    add queue

--- example/test.js
+++ example/test.js
@@ -2,6 +2,30 @@
  * Created by shenzp on 17/12/6.
  */
 
-var cra = require("../index");
+// phantomjs --remote-debugger-port=9000 /Users/shenzp/Works/crawler/example/test.js
+// debugger;
 
-cra.openAddress("http://www.baidu.com");
+var crawler = require("../index");
+
+var cra = new crawler();
+var url = "http://www.xiang5.com/content/13722/594814.html";
+cra.getContent({
+    url: url,
+    content: '.xsDetail',
+    next: "#detailsubsbox > span:nth-child(3) > a",
+});
+
+setInterval(function () {
+    var url = cra.urlQueue.pop();
+    console.log(url);
+    if(url) {
+        cra.getContent({
+            url: url,
+            content: '.xsDetail',
+            next: "#detailsubsbox > span:nth-child(3) > a",
+        });
+    }
+}, 1000);
+
+// cra.ping("http://www.baidu.com");
+
--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 var webPage = require('webpage'),
     system = require('system'),
-    address = system.args[1],
+    Queue = require('./tools/queue');
     jQueryUrl = "http://oxfotmol6.bkt.clouddn.com/jquery-3.1.0/jquery-3.1.0.js";
 
 var page = webPage.create();
@@ -14,78 +14,156 @@ page.onConsoleMessage = function(msg) {
     console.log(msg);
 };
 
-module.exports = {
-    self: this,
-    urlList: [],
-    resultList: [],
-    getContent: function (obj, func) {
+function crawler() {
+    var self = this;
+
+    this.urlQueue = new Queue();
+    this.resultList = [];
+
+    self.getContent = function (obj, func) {
         var url = obj.url,
             next,
             content;
 
-        next = self.paramToArray(obj['next']);
-        content = self.paramToArray(obj['content']);
+        next = paramToArray(obj['next']);
+        content = paramToArray(obj['content']);
 
         if(typeof url !== "string") {
             console.error("parameter `url` need to be type of `string`");
             return;
         }
-        self.openAddress();
-    },
-    paramToArray: function(para) {
-        if(para) {
-            if(para instanceof Array) {
-                return para;
-            } else if(typeof obj['package'] === "string") {
-                return [para];
+        self.openAddress(url, next, content, func);
+    };
+
+    self.ping = function(url) {
+        page.open(url, function (status) {
+            console.log(status);
+            if(status == "success") {
+                page.evaluate(function (s) {
+                    console.log(s);
+                    function a() {
+                        console.log("aaa");
+                    }
+                    a();
+                }, "title");
             }
-        }
-    },
-    openAddress: function(url, next, content, func) {
+        });
+    };
+
+    self.openAddress = function(url, next, content, func) {
         var contentObj = {
             url: url,
             next: next,
             content: content,
             value: []
         };
+
         page.open(url, function (status) {
+            console.log(status);
             if(status === "success") {
-                var jQuery = page.evaluate(function () {
-                    return $;
-                });
-                if(jQuery) {
-                    page.evaluate(function () {
-                        resultHandler();
+                var result = page.evaluate(function (para) {
+                    var para = JSON.parse(para),
+                        urlList = [],
+                        resultList = [];
+
+                    function nextHandler(url, next) {
+                        next.forEach(function (item, i) {
+                            if($(item).attr("href")) {
+                                urlList.push($(item).attr("href"));
+                            } else {
+                                console.log("address:\n", url, "\ndon't has `next` href by " + item)
+                            }
+                        });
+                    }
+
+                    function resultHandler(url, content, value) {
+                        content.forEach(function (item, i) {
+                            if($(item).val()) {
+                                value.push($(item).val());
+                            } else if($(item).html()) {
+                                value.push($(item).html());
+                            } else {
+                                value.push(null);
+                                console.log("can not get content:\n", url, "\n", "content: ", content);
+                            }
+                            resultList.push(value);
+                        });
+                    }
+                    if($) {
+                        resultHandler(para.url, para.content, para.value);
+                        nextHandler(para.url, para.next);
+                        return JSON.stringify({
+                            resultList: resultList,
+                            urlList: urlList
+                        });
+                    } else {
+                        return false;
+                    }
+                }, JSON.stringify(contentObj));
+
+                if(result !== "false") {
+                    result = JSON.parse(result);
+                    contentObj.value = result.resultList;
+                    result.urlList.forEach(function (item, i) {
+                        self.urlQueue.push(item);
                     });
+                    self.resultList.push(contentObj);
                 } else {
                     page.includeJs(jQueryUrl, function() {
                         if(func && typeof func === "function") {
                             page.evaluate(func);
                         } else {
                             page.evaluate(function () {
-                                resultHandler();
+                                resultHandler(url, content, contentObj);
+                                nextHandler(url, next);
                             });
                         }
-                        phantom.exit()
+                        // phantom.exit();
                     });
                 }
             } else {
-                console.log('FAIL to load the address\n', address);
+                console.error('FAIL to load the address\n', url);
             }
         });
+    };
 
-        function resultHandler() {
-            content.forEach(function (item, i) {
-                if($(item).val()) {
-                    contentObj.value.push($(item).val());
-                } else if($(item.html())) {
-                    contentObj.value.push($(item).html());
-                } else {
-                    contentObj.value.push(null);
-                    console.log("can not get content:\n", url, "\n", "content: ", content);
-                }
-                self.resultList.push(contentObj);
-            });
+    function nextHandler(url, next) {
+        next.forEach(function (item, i) {
+            if($(item).attr("href")) {
+                self.urlList.push($(item).attr("href"));
+            } else {
+                console.log("address:\n", url, "\ndon't has `next` href by " + item)
+            }
+        });
+    }
+
+    function resultHandler(url, content, contentObj) {
+        console.log(url);
+        content.forEach(function (item, i) {
+            console.log("item: ", item);
+            if($(item).val()) {
+                console.log($(item).val());
+                contentObj.value.push($(item).val());
+            } else if($(item.html())) {
+                console.log($(item).html());
+                contentObj.value.push($(item).html());
+            } else {
+                contentObj.value.push(null);
+                console.log("can not get content:\n", url, "\n", "content: ", content);
+            }
+            self.resultList.push(contentObj);
+        });
+    }
+
+    function paramToArray(para) {
+        if(para) {
+            if(para instanceof Array) {
+                return para;
+            } else if(typeof para === "string") {
+                return [para];
+            }
         }
     }
-};
+}
+
+module.exports = crawler;
